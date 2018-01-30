83ccedb4461c98003c32115419799ebd9f0446e6
Stefan Werner <stefan_werner88@web.de>
2018-01-30 16:08:02 +0100

    Bester Stand

--- src/tud/ke/ml/project/classifier/NearestNeighbor.java
+++ src/tud/ke/ml/project/classifier/NearestNeighbor.java
@@ -40,12 +40,12 @@ public class NearestNeighbor extends INearestNeighbor implements Serializable {
 		Map<Object, Double> unweightedVotes = new HashMap<Object, Double>();
 		
 		for(Pair<List<Object>,Double> subsetInstance : subset) {
-			Object instanceClass = subsetInstance.getA().get(subsetInstance.getA().size() -1); 
+			Object instanceClass = subsetInstance.getA().get(getClassAttribute()); 
 			
 			if(!unweightedVotes.containsKey(instanceClass))
 					unweightedVotes.put(instanceClass, 0.0);
 			
-			unweightedVotes.put(instanceClass, unweightedVotes.get(instanceClass) + 1);
+			unweightedVotes.put(instanceClass, unweightedVotes.get(instanceClass) + 1d);
 		}
 		
 		return unweightedVotes;
@@ -56,7 +56,7 @@ public class NearestNeighbor extends INearestNeighbor implements Serializable {
 		Map<Object, Double> weightedVotes = new HashMap<Object, Double>();
 		
 		for(Pair<List<Object>,Double> subsetInstance : subset) {
-			Object instanceClass = subsetInstance.getA().get(subsetInstance.getA().size() -1); 
+			Object instanceClass = subsetInstance.getA().get(getClassAttribute()); 
 			
 			if(!weightedVotes.containsKey(instanceClass))
 					weightedVotes.put(instanceClass, 0.0);
@@ -73,7 +73,7 @@ public class NearestNeighbor extends INearestNeighbor implements Serializable {
 		//======================================================================
 		//return winner class
 		//======================================================================
-		double maxCount = 0.0;    								//Count of winner class(es)
+		double maxCount = -1.0;    								//Count of winner class(es)
 		List<Object> winnerClasses = new ArrayList<Object>();	//Class(es) with highest count
 		
 		//======================================================================
@@ -94,7 +94,6 @@ public class NearestNeighbor extends INearestNeighbor implements Serializable {
 		//More than one class has highest votes -> decide for class with highest 
 		//Occurrence(prior) / else decide for the one class
 		//======================================================================
-		/*
 		if (winnerClasses.size() > 1) {
 			Map<Object, Integer> priors = new HashMap<Object, Integer>();
 			//======================================================================
@@ -105,7 +104,7 @@ public class NearestNeighbor extends INearestNeighbor implements Serializable {
 			//=================================================================
 			//Iterate through instances and count if class is candidate class
 			for (List<Object> instance : this.m_data) {
-				Object instanceClass = instance.get(instance.size() -1);	
+				Object instanceClass = instance.get(getClassAttribute());	
 				if(priors.containsKey(instanceClass))
 					priors.put(instanceClass, priors.get(instanceClass) + 1);
 			}
@@ -125,7 +124,7 @@ public class NearestNeighbor extends INearestNeighbor implements Serializable {
 			
 		} 	//else return winner class if only one
 		
-		else */ return winnerClasses.get(0);
+		else return winnerClasses.get(0);
 	}
 
 	@Override
@@ -148,16 +147,14 @@ public class NearestNeighbor extends INearestNeighbor implements Serializable {
 		//Calculate Distance of every model instance with parameter instance "data"
 		//======================================================================
 		for(List<Object> instance : m_data) {
-			
 			double distance;
 			
 			//Normalizing instance/data - Attributes
 			for(int i = 0; i < instance.size(); i++) {
-				if(!(data.get(i) instanceof String))
+				if(data.get(i) instanceof Double) {
 					instance.set(i, ((Double)instance.get(i) + translation[i])*scaling[i]);
-				
-				if(!(data.get(i) instanceof String))
-					data.set(i, ((Double)data.get(i) + translation[i])*scaling[i]);
+					data    .set(i, ((Double)data    .get(i) + translation[i])*scaling[i]);
+				}
 			}
 			
 			if(getMetric() == 0) distance = determineManhattanDistance(instance, data);
@@ -169,7 +166,7 @@ public class NearestNeighbor extends INearestNeighbor implements Serializable {
 			pairs.add(pair);
 		}
 		//======================================================================
-		//Sort List and get first 5 instances (lowest distance)
+		//Sort List and get first k instances (lowest distance)
 		//======================================================================
 		Collections.sort(pairs, 
 			new Comparator<Pair<List<Object>, Double>>() {
@@ -186,7 +183,6 @@ public class NearestNeighbor extends INearestNeighbor implements Serializable {
 		LinkedList<Pair<List<Object>, Double>> nearest =
 				new LinkedList< Pair< List<Object> , Double >>();
 		
-		//Check if size of Pairs is less than 5 (neccesary? discuss!! :D)
 		int bestK = this.getkNearest();
 		
 		for(int i = 0; i < bestK; i++) {
@@ -206,13 +202,13 @@ public class NearestNeighbor extends INearestNeighbor implements Serializable {
 		 
 		//Check if attribute-types match and cast them to types for calculation
 		for (int i = 0; i < instance2.size(); i++) {
-			double difference = 0.0;
+			double difference = 0.0d;
 			//Do not count Class-Attribute
 			if(i == this.getClassAttribute()) continue;
 			
 			if (instance1.get(i) instanceof String && instance2.get(i) instanceof String) {
-				if (((String)instance1.get(i)).equals((String)instance2.get(i))) difference = 0;
-				else difference = 1;
+				if (((String)instance1.get(i)).equals((String)instance2.get(i))) difference = 0d;
+				else difference = 1d;
 			}
 			else if (instance1.get(i) instanceof Double && instance2.get(i) instanceof Double) {
 				difference = Math.abs((Double) instance1.get(i) - (Double) instance2.get(i));
@@ -242,11 +238,11 @@ public class NearestNeighbor extends INearestNeighbor implements Serializable {
 				else attribute_dist = 1;
 			}
 			else if (instance1.get(i) instanceof Double && instance2.get(i) instanceof Double) {
-				attribute_dist = Math.abs((Double) instance1.get(i) - (Double) instance2.get(i));
+				attribute_dist = Math.pow((Double) instance1.get(i) - (Double) instance2.get(i),2);
 			}
 			else throw new IllegalArgumentException("class of attributes does not match");
 			
-			euclidean_dist += Math.pow(attribute_dist, 2);
+			euclidean_dist += attribute_dist;
 		}
 
 		return Math.sqrt(euclidean_dist);
@@ -254,28 +250,31 @@ public class NearestNeighbor extends INearestNeighbor implements Serializable {
 
 	@Override
 	protected double[][] normalizationScaling() {
-		double st[][] = new double[m_data.get(0).size()-1][m_data.get(0).size()-1];
+		int attributeCount = m_data.get(0).size();
+		double st[][] = new double[2][m_data.get(0).size()];
 
-		double minValues[] = new double[m_data.get(0).size()-1];
-		double maxValues[] = new double[m_data.get(0).size()-1];
+		double minValues[] = new double[attributeCount];
+		double maxValues[] = new double[attributeCount];
 		if(isNormalizing()) {
+			//Iterate through instances
 			for(int i = 0; i < m_data.size() - 1; i++) {
-				for(int j = 0; j < m_data.get(0).size() - 1; j++) {
-					if(!(m_data.get(i).get(j) instanceof String)) {
-						
+				//Iterate through attributes
+				for(int j = 0; j < attributeCount - 1; j++) {
+					if(m_data.get(i).get(j) instanceof Double) {
 						//Test -> minValue < als Value der Instanz
 						if((Double)m_data.get(i).get(j) < minValues[j]) {
 							minValues[j] = (Double)m_data.get(i).get(j);
 						}
-						//Test -> maxValue > als Value der Instanz
-						if((Double)m_data.get(i).get(j) > maxValues[j]) maxValues[j] = (Double)m_data.get(i).get(j);
+						//Test -> maxValue > als Value des Attributs der Instanz
+						if((Double)m_data.get(i).get(j) > maxValues[j]) 
+							maxValues[j] = (Double)m_data.get(i).get(j);
 					}
 				}
 			}
 		}
 		//Fill scaling and translation array
-		for(int j = 0; j < m_data.get(0).size() - 1; j++) {
-				st[0][j] = minValues[j] == 0 ? 0 : (Double)(1 / (maxValues[j] - minValues[j]));
+		for(int j = 0; j < attributeCount - 1; j++) {
+				st[0][j] = (maxValues[j] - minValues[j]) == 0 ? 0 : (Double)(1 / (maxValues[j] - minValues[j]));
 				st[1][j] = -1 * minValues[j];
 		}
 		return st;
